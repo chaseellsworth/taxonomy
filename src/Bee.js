d737b768ec693af1fc4eff6de711f3068ad43d83
Chase Ellsworth <chaseme3@gmail.com>
2014-11-03 14:03:08 -0800

    finished repo

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,28 @@
 var Bee = function() {
+	Grub.call(this);
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = "Keep on growing";	
 };
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
+
+
+// // meanwhile in my js code 
+// var myBee = new Bee()
+// // myBee.color // Grub.
+
+// var Bee = function  () {
+// 	// body...
+// // the function object that I am in has a prototype property
+// // set delegation reference of newObj to prototype
+
+// 	var newObj = {};
+// 	newObj.age = 5;
+
+// 	return newObj;
+// }
+
+// var bee = Bee();
+// bee.color
